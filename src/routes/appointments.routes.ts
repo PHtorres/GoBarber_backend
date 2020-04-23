7a28770d5cd1a6cd0fcfddfc54122bae9aadf941
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-04-23 12:21:44 -0300

    Initial commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,27 @@
+import { Router, request, response } from 'express';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+const appointmentsRouter = Router();
+
+const appointments:Array<Appointment> = [];
+
+appointmentsRouter.post('/', (request, response) => {
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointments.find(item => isEqual(item.date, parsedDate));
+
+    if (findAppointmentInSameDate) {
+        return response.status(400).json({ message: 'This appointment has already booked' });
+    }
+
+    const appointment = new Appointment(provider, parsedDate);
+
+    appointments.push(appointment);
+
+    return response.json(appointment);
+})
+
+export default appointmentsRouter;
