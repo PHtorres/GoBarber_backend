46bfeee827abb1f1a3f35949a552baa56f4d5b26
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-05-02 17:09:50 -0300

    aplicando rpositorio com banco de dados

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,31 +1,16 @@
-import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
+import {Entity, EntityRepository, Repository} from 'typeorm';
 
-interface CreateAppointmentDTO{
-    provider: string;
-    date: Date
-}
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
 
-class AppointmentsRepository{
-    private appointments: Array<Appointment>;
+    public async findByDate(date: Date): Promise<Appointment | null>{
 
-    constructor(){
-        this.appointments = [];
-    }
+        const findAppointment = await this.findOne({
+            where:{date}
+        });
 
-    public all():Array<Appointment>{
-        return this.appointments;
-    }
-
-    public findByDate(date: Date):Appointment | null{
-        const findAppointment = this.appointments.find(item => isEqual(item.date, date));
         return findAppointment || null;
-    }
-
-    public create({provider, date}: CreateAppointmentDTO):Appointment{
-        const appointment = new Appointment({provider, date});
-        this.appointments.push(appointment);
-        return appointment;
     }
 }
 
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,24 +1,24 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import {getCustomRepository} from 'typeorm';
 import CreateAppoitmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-
-appointmentsRouter.get('/', (request, response) => {
-    const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointments = await appointmentsRepository.find()
     return response.json(appointments);
 });
 
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
     try {
         const { provider, date } = request.body;
         const parsedDate = parseISO(date);
-        const createAppointmentService = new CreateAppoitmentService(appointmentsRepository);
-        const appointment = createAppointmentService.execute({ provider, date: parsedDate });
+        const createAppointmentService = new CreateAppoitmentService();
+        const appointment = await createAppointmentService.execute({ provider, date: parsedDate });
         return response.json(appointment);
     } catch (e) {
         return response.status(400).json({ error: e.message });
