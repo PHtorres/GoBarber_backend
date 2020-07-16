edf2c4a420a9d14ad6071594b645bb4a23c96946
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-07-16 01:52:46 -0300

    Criando serviço para listar disponibilidade no mês para provider além de testes

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,27 +1,43 @@
 import Appointment from '../entities/Appointment';
 import ICreateAppointmentDTO from '../../../dtos/ICreateAppointmentDTO';
 import IAppointmenteRepository from '../../../repositories/IAppointmentsRepository';
-import {getRepository, Repository} from 'typeorm';
+import { getRepository, Repository, Raw } from 'typeorm';
+import IFindAllInMonthFromProviderDTO from '../../../dtos/IFindAllInMonthFromProviderDTO';
 
 class AppointmentsRepository implements IAppointmenteRepository {
 
     private ormRepository: Repository<Appointment>;
 
-    constructor(){
+    constructor() {
         this.ormRepository = getRepository(Appointment);
     }
 
-    public async findByDate(date: Date): Promise<Appointment | undefined>{
+    public async findByDate(date: Date): Promise<Appointment | undefined> {
 
         const findAppointment = await this.ormRepository.findOne({
-            where:{date}
+            where: { date }
         });
 
         return findAppointment;
     }
 
-    public async create({date, provider_id}:ICreateAppointmentDTO):Promise<Appointment>{
-        const appointment = this.ormRepository.create({provider_id, date});
+    public async findAllInMonthFromProvider({ provider_id, year, month }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+
+        const parsedMonth = String(month).padStart(2, '0');
+
+        const appointments = await this.ormRepository.find({
+            where: {
+                provider_id,
+                date: Raw(dateFieldName =>
+                    `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`)
+            }
+        });
+
+        return appointments;
+    }
+
+    public async create({ date, provider_id }: ICreateAppointmentDTO): Promise<Appointment> {
+        const appointment = this.ormRepository.create({ provider_id, date });
         await this.ormRepository.save(appointment);
         return appointment;
     }
--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,20 +1,31 @@
-import {uuid} from 'uuidv4';
-import {isEqual} from 'date-fns';
+import { uuid } from 'uuidv4';
+import { isEqual, getMonth, getYear } from 'date-fns';
 
 import Appointment from '../../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../../dtos/ICreateAppointmentDTO';
 import IAppointmenteRepository from '../IAppointmentsRepository';
+import IFindAllInMonthFromProviderDTO from '../../dtos/IFindAllInMonthFromProviderDTO';
 
 class AppointmentsRepository implements IAppointmenteRepository {
 
-    private appointments:Appointment[] = [];
+    private appointments: Appointment[] = [];
 
-    public async findByDate(date: Date): Promise<Appointment | undefined>{
+    public async findByDate(date: Date): Promise<Appointment | undefined> {
         const findAppointment = this.appointments.find(appointment => isEqual(appointment.date, date));
         return findAppointment;
     }
 
-    public async create({date, provider_id}:ICreateAppointmentDTO):Promise<Appointment>{
+    public async findAllInMonthFromProvider({ provider_id, month, year }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+
+        const appointments = this.appointments.filter(item =>
+            item.provider_id === provider_id &&
+            getMonth(item.date) + 1 === month &&
+            getYear(item.date) === year);
+
+        return appointments;
+    }
+
+    public async create({ date, provider_id }: ICreateAppointmentDTO): Promise<Appointment> {
         const appointment = new Appointment();
         appointment.id = uuid();
         appointment.date = date;
